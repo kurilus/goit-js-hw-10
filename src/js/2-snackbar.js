de28630aed2455d167c42eec8b6a102e6cf13842
Kyrylo Pavlov <krlpvlv13@gmail.com>
2025-06-04 21:54:20 +0300

    complete task 2 js

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.form');
+  
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+  
+      const delay = Number(form.elements.delay.value);
+      const state = form.elements.state.value;
+  
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (state === 'fulfilled') {
+            resolve(delay);
+          } else {
+            reject(delay);
+          }
+        }, delay);
+      })
+        .then(delay => {
+          iziToast.success({
+            title: 'Success',
+            message: `✅ Fulfilled promise in ${delay}ms`,
+            position: 'topRight',
+          });
+        })
+        .catch(delay => {
+          iziToast.error({
+            title: 'Error',
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: 'topRight',
+          });
+        });
+    });
+  });
