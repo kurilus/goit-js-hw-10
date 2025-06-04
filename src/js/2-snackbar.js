66371f3e4682ffed390f57f191a71e4e57ed106b
Kyrylo Pavlov <krlpvlv13@gmail.com>
2025-06-04 22:47:48 +0300

    task 2 styled

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,34 +1,72 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.form');
-  
-    form.addEventListener('submit', event => {
-      event.preventDefault();
-  
-      const delay = Number(form.elements.delay.value);
-      const state = form.elements.state.value;
-  
-      new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (state === 'fulfilled') {
-            resolve(delay);
-          } else {
-            reject(delay);
-          }
-        }, delay);
-      })
-        .then(delay => {
-          iziToast.success({
-            title: 'Success',
-            message: `✅ Fulfilled promise in ${delay}ms`,
-            position: 'topRight',
-          });
-        })
-        .catch(delay => {
-          iziToast.error({
-            title: 'Error',
-            message: `❌ Rejected promise in ${delay}ms`,
-            position: 'topRight',
-          });
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delayInput = form.elements.delay;
+    const delayValue = delayInput.value.trim();
+    const stateValue = form.elements.state.value;
+
+    if (!delayValue) {
+      iziToast.warning({
+        title: 'Caution',
+        message: 'You forgot important data',
+        position: 'topRight',
+        backgroundColor: '#ffa000',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        iconUrl: './img/caution-icon.svg',
+      });
+      delayInput.focus();
+      return;
+    }
+
+    if (!stateValue) {
+      iziToast.warning({
+        title: 'Caution',
+        message: 'You forgot important data',
+        position: 'topRight',
+        backgroundColor: '#ffa000',
+        titleColor: '#fff',
+        messageColor: '#fff',
+        iconUrl: './img/caution-icon.svg',
+      });
+      return;
+    }
+
+    const delay = Number(delayValue);
+
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (stateValue === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    })
+      .then(delay => {
+        iziToast.success({
+          title: 'OK',
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight',
+          backgroundColor: '#59a10d',
+          titleColor: '#fff',
+          messageColor: '#fff',
+          iconUrl: './img/success-icon.svg',
         });
-    });
+      })
+      .catch(delay => {
+        iziToast.error({
+          title: 'Error',
+          message: `Illegal operation`,
+          position: 'topRight',
+          backgroundColor: '#ef4040',
+          titleColor: '#fff',
+          messageColor: '#fff',
+          iconUrl: './img/error-icon.svg',
+        });
+      });
   });
+});
